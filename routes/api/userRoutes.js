6ed450b9d53d213102d8335c3841dd1e773fdac2
i-am-shantosh <e19371@eng.pdn.ac.lk>
2024-01-22 08:08:01 +0530

    Update userRoute.js

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -30,9 +30,7 @@ module.exports = (app) => {
                   console.log(err);
                   res.status(500).send("Internal Server Error");
                 } else {
-                  res.json({
-                    token,
-                  });
+                  res.send(token);
                 }
               }
             );
